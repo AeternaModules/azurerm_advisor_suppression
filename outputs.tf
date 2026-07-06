21d95a2462c85fde330665edb65beafb944f3974
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:10:45 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,24 @@ output "advisor_suppressions" {
   description = "All advisor_suppression resources"
   value       = azurerm_advisor_suppression.advisor_suppressions
 }
+output "advisor_suppressions_name" {
+  description = "List of name values across all advisor_suppressions"
+  value       = [for k, v in azurerm_advisor_suppression.advisor_suppressions : v.name]
+}
+output "advisor_suppressions_recommendation_id" {
+  description = "List of recommendation_id values across all advisor_suppressions"
+  value       = [for k, v in azurerm_advisor_suppression.advisor_suppressions : v.recommendation_id]
+}
+output "advisor_suppressions_resource_id" {
+  description = "List of resource_id values across all advisor_suppressions"
+  value       = [for k, v in azurerm_advisor_suppression.advisor_suppressions : v.resource_id]
+}
+output "advisor_suppressions_suppression_id" {
+  description = "List of suppression_id values across all advisor_suppressions"
+  value       = [for k, v in azurerm_advisor_suppression.advisor_suppressions : v.suppression_id]
+}
+output "advisor_suppressions_ttl" {
+  description = "List of ttl values across all advisor_suppressions"
+  value       = [for k, v in azurerm_advisor_suppression.advisor_suppressions : v.ttl]
+}
 
