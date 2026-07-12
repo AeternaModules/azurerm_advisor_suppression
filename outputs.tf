f05c0603a764187a7c96dd759a95b383cff02f62
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:20 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "advisor_suppressions" {
-  description = "All advisor_suppression resources"
-  value       = azurerm_advisor_suppression.advisor_suppressions
-}
 output "advisor_suppressions_name" {
-  description = "List of name values across all advisor_suppressions"
-  value       = [for k, v in azurerm_advisor_suppression.advisor_suppressions : v.name]
+  description = "Map of name values across all advisor_suppressions, keyed the same as var.advisor_suppressions"
+  value       = { for k, v in azurerm_advisor_suppression.advisor_suppressions : k => v.name }
 }
 output "advisor_suppressions_recommendation_id" {
-  description = "List of recommendation_id values across all advisor_suppressions"
-  value       = [for k, v in azurerm_advisor_suppression.advisor_suppressions : v.recommendation_id]
+  description = "Map of recommendation_id values across all advisor_suppressions, keyed the same as var.advisor_suppressions"
+  value       = { for k, v in azurerm_advisor_suppression.advisor_suppressions : k => v.recommendation_id }
 }
 output "advisor_suppressions_resource_id" {
-  description = "List of resource_id values across all advisor_suppressions"
-  value       = [for k, v in azurerm_advisor_suppression.advisor_suppressions : v.resource_id]
+  description = "Map of resource_id values across all advisor_suppressions, keyed the same as var.advisor_suppressions"
+  value       = { for k, v in azurerm_advisor_suppression.advisor_suppressions : k => v.resource_id }
 }
 output "advisor_suppressions_suppression_id" {
-  description = "List of suppression_id values across all advisor_suppressions"
-  value       = [for k, v in azurerm_advisor_suppression.advisor_suppressions : v.suppression_id]
+  description = "Map of suppression_id values across all advisor_suppressions, keyed the same as var.advisor_suppressions"
+  value       = { for k, v in azurerm_advisor_suppression.advisor_suppressions : k => v.suppression_id }
 }
 output "advisor_suppressions_ttl" {
-  description = "List of ttl values across all advisor_suppressions"
-  value       = [for k, v in azurerm_advisor_suppression.advisor_suppressions : v.ttl]
+  description = "Map of ttl values across all advisor_suppressions, keyed the same as var.advisor_suppressions"
+  value       = { for k, v in azurerm_advisor_suppression.advisor_suppressions : k => v.ttl }
 }
 
